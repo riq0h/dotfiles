e8458f455f6456630e09ac9c7b36d28ba6cc0575
Rikuoh Tsujitani <mail@riq0h.jp>
2023-11-23 13:01:38 +0900

    ln

---
+++ lnsv.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -s /home/riq0h/dotfiles/.svzshrc ~/.zshrc
+mkdir .config/nvim
+ln -s /home/riq0h/dotfiles/init.lua ~/.config/nvim/init.lua
+ln -s /home/riq0h/dotfiles/.zpreztorc ~/.zpreztorc
+ln -s /home/riq0h/.zprezto ~/.zprezto
