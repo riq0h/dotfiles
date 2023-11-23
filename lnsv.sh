6c099051bff5a4c5286df84786da7f048d2e91e6
Rikuoh Tsujitani <mail@riq0h.jp>
2023-11-23 14:12:35 +0900

    fix

--- lnsv.sh
+++ lnsv.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-ln -s /home/riq0h/dotfiles/.svzshrc ~/.zshrc
+ln -sf /home/riq0h/dotfiles/.svzshrc ~/.zshrc
+mkdir .config
 mkdir .config/nvim
-ln -s /home/riq0h/dotfiles/init.lua ~/.config/nvim/init.lua
-ln -s /home/riq0h/dotfiles/.zpreztorc ~/.zpreztorc
-ln -s /home/riq0h/.zprezto ~/.zprezto
+ln -sf /home/riq0h/dotfiles/init.lua ~/.config/nvim/init.lua
+ln -sf /home/riq0h/dotfiles/.zpreztorc ~/.zpreztorc
+ln -sf /home/riq0h/.zprezto ~/.zprezto
