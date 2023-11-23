4c42e733582150f2b3d41a7e3a89d62819e18545
Rikuoh <mail@riq0h.jp>
2023-11-23 21:33:26 +0900

    fix

--- lnsv.sh
+++ lnsv.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 ln -sf /home/riq0h/dotfiles/.svzshrc ~/.zshrc
-mkdir .config
-mkdir .config/nvim
+mkdir -p .config/nvim
 ln -sf /home/riq0h/dotfiles/init.lua ~/.config/nvim/init.lua
 ln -sf /home/riq0h/dotfiles/.zpreztorc ~/.zpreztorc
 ln -sf /home/riq0h/.zprezto ~/.zprezto
