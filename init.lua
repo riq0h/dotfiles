b434bb76f4c82a9632936e57fe9e4176ff5a3eb3
Rikuoh <mail@riq0h.jp>
2024-05-10 22:05:20 +0900

    test new comp key

--- init.lua
+++ init.lua
@@ -637,8 +637,8 @@ cmp.setup({
 	},
 
 	mapping = cmp.mapping.preset.insert({
-		["<Tab>"] = cmp.mapping.select_next_item(),
-		["<S-Tab>"] = cmp.mapping.select_prev_item(),
+		["<Space>"] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
