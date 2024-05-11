46f6ff8f381e410bc762ad65f9b78aab78d58881
Rikuoh Tsujitani <mail@riq0h.jp>
2024-05-11 09:39:23 +0900

    test keymap #2

--- init.lua
+++ init.lua
@@ -637,7 +637,7 @@ cmp.setup({
 	},
 
 	mapping = cmp.mapping.preset.insert({
-		["<Space>"] = cmp.mapping.select_next_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-p>"] = cmp.mapping.select_prev_item(),
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
