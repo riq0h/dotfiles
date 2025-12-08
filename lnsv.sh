e3a503dd1f8ff03167b200e9dd4eddd2986ea8f0
Rikuoh Tsujitani <mail@riq0h.jp>
2025-12-08 15:44:32 +0900

    for ssh

--- lnsv.sh
+++ lnsv.sh
@@ -2,7 +2,7 @@
 
 ln -sf /home/riq0h/dotfiles/.svzshrc ~/.zshrc
 mkdir -p .config/nvim
-ln -sf /home/riq0h/dotfiles/init.lua ~/.config/nvim/init.lua
+ln -sf /home/riq0h/dotfiles/init_ssh.lua ~/.config/nvim/init.lua
 ln -sf /home/riq0h/dotfiles/.zpreztorc ~/.zpreztorc
 ln -sf /home/riq0h/.zprezto ~/.zprezto
 ln -sf /.local/share/nvim ~/.local/share/nvim
